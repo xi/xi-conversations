a5f48cbc5b633155b60b7f3ea0ee14b07d3ea196
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-03-31 00:57:12 +0200

    show full conversation

--- content/main.js
+++ content/main.js
@@ -1,4 +1,5 @@
 Components.utils.import('resource://gre/modules/Services.jsm');
+Components.utils.import("resource:///modules/gloda/gloda.js");
 var Messenger = Components.classes['@mozilla.org/messenger;1'].createInstance(Components.interfaces.nsIMessenger);
 
 var getParams = function() {
@@ -17,13 +18,47 @@ var uri2url = function(uri) {
 	return _output.value.spec;
 };
 
+var uri2msg = function(uri) {
+	var messageService = Messenger.messageServiceFromURI(uri);
+	return messageService.messageURIToMsgHdr(uri);
+};
+
 var msg2uri = function(msg) {
 	return msg.folder.getUriForMsg(msg);
 };
 
-var createMessageElement = function(msg) {
+var getConversation = function(msgs, cb) {
+	var done = false;
+
+	var conversationListener = {
+		onItemsAdded: function() {},
+		onItemsModified: function() {},
+		onItemsRemoved: function() {},
+		onQueryCompleted: function(collection) {
+			if (!done) {
+				done = true;
+				cb(collection.items);
+			}
+		}
+	};
+
+	var listener = {
+		onItemsAdded: function(items) {
+			if (items.length >= 1) {
+				items[0].conversation.getMessagesCollection(conversationListener, true);
+			}
+		},
+		onItemsModified: function() {},
+		onItemsRemoved: function() {},
+		onQueryCompleted: function() {}
+	};
+
+	Gloda.getMessageCollectionForHeaders(msgs, listener, null);
+};
+
+var createMessageElement = function(glodaMsg) {
 	var iframe = document.createElement('iframe');
-	iframe.src = uri2url(msg2uri(msg));
+	iframe.src = uri2url(msg2uri(glodaMsg.folderMessage));
 	iframe.className = 'message__body';
 	iframe.addEventListener('DOMContentLoaded', function() {
 		iframe.style.height = iframe.contentDocument.body.scrollHeight + 'px';
@@ -32,8 +67,11 @@ var createMessageElement = function(msg) {
 };
 
 var initialUrls = (getParams().urls || '').split(',');
+var initialMsgs = initialUrls.map(uri2msg);
 
-var container = document.querySelector('.conversation__main');
-for (let uri of initialUrls) {
-	container.appendChild(createMessageElement(msg));
-}
+getConversation(initialMsgs, function(conversation) {
+	var container = document.querySelector('.conversation__main');
+	for (let glodaMsg of conversation) {
+		container.appendChild(createMessageElement(glodaMsg));
+	}
+});
