05fb602b451a520ed6a0b234e7444bb4f4efa7a1
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-03-31 11:14:55 +0200

    performance: lazy load iframe

--- content/elements.js
+++ content/elements.js
@@ -176,13 +176,23 @@ var createMessageElement = function(glodaMsg, expanded) {
 		e.classList.add('is-collapsed');
 	}
 
+	var iframeLoaded = false;
+	var lazyLoadIframe = function() {
+		if (!iframeLoaded && !e.classList.contains('is-collapsed')) {
+			e.appendChild(createIframe(glodaMsg));
+			iframeLoaded = true;
+		}
+	};
+
 	var header = createMessageHeader(glodaMsg);
 	header.addEventListener('click', function(event) {
 		event.preventDefault();
 		e.classList.toggle('is-collapsed');
+		lazyLoadIframe();
 	});
 	e.appendChild(header);
 
-	e.appendChild(createIframe(glodaMsg));
+	lazyLoadIframe();
+
 	return e;
 };
