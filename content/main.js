6381829fe5f0f51e102887a1de5afb5bb020e1c3
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-03-31 02:04:53 +0200

    define actions

--- content/main.js
+++ content/main.js
@@ -57,6 +57,20 @@ var getConversation = function(msgs, cb) {
 	Gloda.getMessageCollectionForHeaders(msgs, listener, null);
 };
 
+var compose = function(compType) {
+	return function(msg) {
+		var mainWindow = window.frameElement.ownerDocument.defaultView;
+		mainWindow.ComposeMessage(compType, Ci.nsIMsgCompFormat.Default, msg, [msg2uri(msg)]);
+	};
+};
+
+var replyToSender = compose(Components.interfaces.nsIMsgCompType.ReplyToSender);
+var replyAll = compose(Components.interfaces.nsIMsgCompType.ReplyAll);
+var replyToList = compose(Components.interfaces.nsIMsgCompType.ReplyToList);
+var editAsNew = compose(Components.interfaces.nsIMsgCompType.Template);
+var editDraft = compose(Components.interfaces.nsIMsgCompType.Draft);
+var forward = compose(Components.interfaces.nsIMsgCompType.ForwardInline);
+
 var getTags = function(msg) {
 	var keywords = msg.getStringProperty('keywords');
 	var keywordList = keywords.split(' ');
