0249c2f57f368c3be889b97cfd37a896de3143db
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-04-01 10:48:48 +0200

    simplify attachment

--- content/footer.js
+++ content/footer.js
@@ -8,17 +8,15 @@ var createMessageFooter = function(glodaMsg) {
 	var attachments = document.createElement('ul');
 	attachments.className = 'attachments';
 	for (let att of glodaMsg.attachmentInfos) {
-		let attInfo = new mainWindow.AttachmentInfo(att.contentType, att.url, att.name, msg2uri(msg), att.isExternal, 42);
 		let li = document.createElement('li');
-		li.className = 'attachment';
-		li.textContent = att.name;
-		li.appendChild(createActionButton(attInfo, 'open', 'x-open_in_new', function(attInfo) {
-			attInfo.open();
-		}));
-		li.appendChild(createActionButton(attInfo, 'save', 'file', function(attInfo) {
-			attInfo.save();
-		}));
 		attachments.appendChild(li);
+
+		let a = document.createElement('a');
+		a.className = 'attachment';
+		a.textContent = att.name;
+		a.href = att.url;
+		prependChild(a, createIcon('x-attachment'));
+		li.appendChild(a);
 	}
 	footer.appendChild(attachments);
 
