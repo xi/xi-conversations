742046a1cdc57a2aaa73a9e6fd19ca54b135b0b3
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-12-15 09:47:55 +0100

    fix enigmail integration

--- src/js/crypto.js
+++ src/js/crypto.js
@@ -1,7 +1,10 @@
 var util = require('./util.js');
 
+const {EnigmailConstants} = ChromeUtils.import('chrome://enigmail/content/modules/constants.jsm');
+const {EnigmailSingletons} = ChromeUtils.import('chrome://enigmail/content/modules/singletons.jsm');
+
 var parseStatusFlags = function(statusFlags) {
-	var nsIEnigmail = Components.interfaces.nsIEnigmail;
+	var nsIEnigmail = EnigmailConstants;
 
 	var signed = '';
 	if (statusFlags & nsIEnigmail.PGP_MIME_SIGNED) {
@@ -27,22 +30,12 @@ var parseStatusFlags = function(statusFlags) {
 
 var patchSecurityInfo = function() {
 	var mainWindow = window.frameElement.ownerDocument.defaultView;
-	var si = mainWindow.messageHeaderSink.securityInfo;
-	var proto = Object.getPrototypeOf(si);
-	var original = proto.updateSecurityStatus;
-
-	proto.updateSecurityStatus = function(lastMsgUri, exitCode, statusFlags, keyId, userId, sigDetails, errorMsg, blockSeparation, msgUrl, encToDetails) {
-		mainWindow.xiEnigmail.trigger(msgUrl.specIgnoringRef, {
-			lastMsgUri: lastMsgUri,
-			exitCode: exitCode,
+	var headerSink = EnigmailSingletons.messageReader;
+	var original = headerSink.updateSecurityStatus;
+	headerSink.updateSecurityStatus = function(msgUriSpec, exitCode, statusFlags, keyId, userId, sigDetails, errorMsg, blockSeparation, uri, jsonStr, mimePartNumber) {
+		mainWindow.xiEnigmail.trigger(uri.uri, {
 			statusFlags: statusFlags,
-			keyId: keyId,
-			userId: userId,
-			sigDetails: sigDetails,
 			errorMsg: errorMsg,
-			blockSeparation: blockSeparation,
-			msgUrl: msgUrl,
-			encToDetails: encToDetails,
 		});
 
 		original.apply(this, arguments);
@@ -55,12 +48,6 @@ var monkeyPatchCrypto = function() {
 	if (!mainWindow.xiEnigmail) {
 		mainWindow.xiEnigmail = new util.EventService();
 		patchSecurityInfo();
-
-		var original = mainWindow.messageHeaderSink.enigmailPrepSecurityInfo;
-		mainWindow.messageHeaderSink.enigmailPrepSecurityInfo = function() {
-			original.apply(this, arguments);
-			patchSecurityInfo();
-		};
 	}
 };
 
