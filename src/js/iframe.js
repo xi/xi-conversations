e57a15d3db0c8df8ee5b3e6f132b05512dce44d7
Tobias Bengfort <tobias.bengfort@posteo.de>
2020-02-16 15:59:37 +0100

    another attempt at iframe resizing

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -5,9 +5,11 @@ var adjustHeight = function(iframe) {
 	// offsetHeight does not contain margins
 	// scrollHeight does not shrink
 
-	iframe.style.height = 'auto';
-	iframe.getBoundingClientRect();  // force reflow
-	iframe.style.height = iframe.contentDocument.body.scrollHeight + 'px';
+	var body = iframe.contentDocument.body;
+	var style = getComputedStyle(body);
+	var marginTop = parseInt(style.marginTop, 10);
+	var marginBottom = parseInt(style.marginBottom, 10);
+	iframe.style.height = (marginTop + body.offsetHeight + marginBottom)+ 'px';
 };
 
 var hideBlocks = function(iframe, test, hideText, showText, color) {
