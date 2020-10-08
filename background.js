0772f1f2427c51ab3293f01006885d0904101f04
Tobias Bengfort <tobias.bengfort@posteo.de>
2020-10-08 15:08:33 +0200

    mv monkey patch to experiment api

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
 /* global browser */
 
 browser.xi.setup();
+
+browser.xi.onOpenTab.addListener(uris => {
+	browser.tabs.create({url: '/content/main.html?uris=' + encodeURIComponent(uris)});
+});
--- experiment-api/api.js
+++ experiment-api/api.js
@@ -5,6 +5,10 @@ var aomStartup = Components.classes['@mozilla.org/addons/addon-manager-startup;1
 var {ExtensionCommon} = ChromeUtils.import('resource://gre/modules/ExtensionCommon.jsm');
 var {Services} = ChromeUtils.import('resource://gre/modules/Services.jsm');
 
+var msg2uri = function(msg) {
+	return msg.folder.getUriForMsg(msg);
+};
+
 var xi = class extends ExtensionCommon.ExtensionAPI {
 	setup() {
 		var manifestURI = Services.io.newURI('manifest.json', null, this.extension.rootURI);
@@ -15,6 +19,28 @@ var xi = class extends ExtensionCommon.ExtensionAPI {
 		return {
 			xi: {
 				setup: () => this.setup(),
+				onOpenTab: new ExtensionCommon.EventManager({
+					context,
+					name: 'xi.onOpenTab',
+					register(fire) {
+						var observer = (win, topic) => {
+							if (topic === 'domwindowopened' && win.location.href === 'chrome://messenger/content/messenger.xhtml') {
+								win.ThreadPaneDoubleClick = () => {
+									var msgs = win.gFolderDisplay.selectedMessages;
+									fire.async(msgs.map(msg2uri));
+								};
+							}
+							win.addEventListener('load', () => observer(win, topic));
+						};
+
+						Services.ww.registerNotification(observer);
+
+						var e = Services.ww.getWindowEnumerator();
+						while (e.hasMoreElements()) {
+							observer(e.getNext(), 'domwindowopened');
+						}
+					},
+				}).api(),
 			},
 		};
 	}
