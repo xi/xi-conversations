a849fccaf896d48e3cc8e21dc389f5a9e23ad8cc
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-09-06 04:31:03 +0200

    inline template function

--- content/js/message.js
+++ content/js/message.js
@@ -47,36 +47,42 @@ var createAuthor = function(author) {
 	return a;
 };
 
-var template = function(ctx) {
+export default function(msg, expanded) {
 	var h = util.h;
 	var _ = browser.i18n.getMessage;
 
-	return h('article', {'class': ctx.isExpanded ? 'message is-expanded' : 'message', 'id': `msg-${ctx.id}`, 'tabindex': -1}, [
+	var canReplyAll = (
+		util.parseContacts(msg.recipients).length
+		+ util.parseContacts(msg.ccList).length
+		+ util.parseContacts(msg.bccList).length
+	) > 1;
+
+	var e = h('article', {'class': expanded ? 'message is-expanded' : 'message', 'id': `msg-${msg.id}`, 'tabindex': -1}, [
 		h('header', {'class': 'message__header'}, [
-			h('button', {'class': ctx.isFlagged ? 'star is-active' : 'star', 'data-action': 'toggleFlagged'}, [util.createIcon('star')]),
-			createAuthor(ctx.author),
+			h('button', {'class': msg.flagged ? 'star is-active' : 'star', 'data-action': 'toggleFlagged'}, [util.createIcon('star')]),
+			createAuthor(util.parseContacts([msg.author])[0]),
 			' ',
 			h('span', {'class': 'message__recipients'}, [
 				_('to'),
 				' ',
-				...ctx.recipients.map(r => h('a', {'href': `mailto:${r.email}`}, [r.name])),
-				...ctx.cc.map(r => h('a', {'href': `mailto:${r.email}`, 'class': 'message__recipients__cc'}, [r.name])),
-				...ctx.bcc.map(r => h('a', {'href': `mailto:${r.email}`, 'class': 'message__recipients__bcc'}, [r.name])),
+				...util.parseContacts(msg.recipients).map(r => h('a', {'href': `mailto:${r.email}`}, [r.name])),
+				...util.parseContacts(msg.ccList).map(r => h('a', {'href': `mailto:${r.email}`, 'class': 'message__recipients__cc'}, [r.name])),
+				...util.parseContacts(msg.bccList).map(r => h('a', {'href': `mailto:${r.email}`, 'class': 'message__recipients__bcc'}, [r.name])),
 			]),
-			h('span', {'class': 'message__summary'}, [ctx.summary]),
-			ctx.hasAttachments ? util.createIcon('attachment') : null,
-			util.createDate(ctx.date),
+			h('span', {'class': 'message__summary'}, [(msg.body || '').substring(0, 150)]),
+			(msg.attachmentInfos || []).length ? util.createIcon('attachment') : null,
+			util.createDate(msg.date),
 			h('span', {'class': 'message__actions'}, [
-				ctx.canReplyAll
+				canReplyAll
 					? h('button', {'class': 'button', 'title': _('replyAll'), 'data-action': 'replyAll'}, [util.createIcon('reply_all')])
-					: ctx.canReplyToList
+					: msg.canReplyToList
 						? h('button', {'class': 'button', 'title': _('replyList'), 'data-action': 'replyToList'}, [util.createIcon('list')])
 						: h('button', {'class': 'button', 'title': _('reply'), 'data-action': 'replyToSender'}, [util.createIcon('reply')]),
 				h('button', {'class': 'button dropdownToggle', 'title': _('more')}, [util.createIcon('menu')]),
 				h('div', {'class': 'dropdown'}, [
 					h('button', {'class': 'dropdown-item', 'data-action': 'replyToSender'}, [util.createIcon('reply'), ' ', _('reply')]),
-					ctx.canReplyAll ? h('button', {'class': 'dropdown-item', 'data-action': 'replyAll'}, [util.createIcon('reply_all'), ' ', _('replyAll')]) : null,
-					ctx.canReplyToList ? h('button', {'class': 'dropdown-item', 'data-action': 'replyToList'}, [util.createIcon('list'), ' ', _('replyList')]) : null,
+					canReplyAll ? h('button', {'class': 'dropdown-item', 'data-action': 'replyAll'}, [util.createIcon('reply_all'), ' ', _('replyAll')]) : null,
+					msg.canReplyToList ? h('button', {'class': 'dropdown-item', 'data-action': 'replyToList'}, [util.createIcon('list'), ' ', _('replyList')]) : null,
 					h('button', {'class': 'dropdown-item', 'data-action': 'forward'}, [util.createIcon('forward'), ' ', _('forward')]),
 					h('button', {'class': 'dropdown-item', 'data-action': 'editAsNew'}, [util.createIcon('create'), ' ', _('edit')]),
 					h('button', {'class': 'dropdown-item', 'data-action': 'viewClassic'}, [util.createIcon('open_in_new'), ' ', _('viewClassic')]),
@@ -85,38 +91,14 @@ var template = function(ctx) {
 			]),
 		]),
 		h('div', {'class': 'message__details'}, [
-			ctx.isJunk ? util.createAlert(_('junk'), 'mode_heat', 'warning') : null,
+			msg.junk ? util.createAlert(_('junk'), 'mode_heat', 'warning') : null,
 			h('footer', {'class': 'message__footer'}, [
-				h('ul', {'class': 'attachments'}, (ctx.attachments || []).map(a => h('li', {}, [
+				h('ul', {'class': 'attachments'}, (msg.attachmentInfos || []).map(a => h('li', {}, [
 					h('a', {'class': 'attachment', 'href': a.url}, [util.createIcon('attachment'), ' ', a.name]),
 				]))),
 			]),
 		]),
 	]);
-};
-
-export default function(msg, expanded) {
-	var e = template({
-		id: msg.id,
-		isExpanded: expanded,
-		isFlagged: msg.flagged,
-		isJunk: msg.junk,
-		isEncrypted: msg.isEncrypted,
-		author: util.parseContacts([msg.author])[0],
-		recipients: util.parseContacts(msg.recipients),
-		cc: util.parseContacts(msg.ccList),
-		bcc: util.parseContacts(msg.bccList),
-		summary: (msg.body || '').substring(0, 150),
-		attachments: msg.attachmentInfos,
-		hasAttachments: (msg.attachmentInfos || []).length,
-		date: msg.date,
-		canReplyToList: msg.canReplyToList,
-		canReplyAll: (
-			util.parseContacts(msg.recipients).length
-			+ util.parseContacts(msg.ccList).length
-			+ util.parseContacts(msg.bccList).length
-		) > 1,
-	});
 
 	autoMarkAsRead(e, msg);
 
