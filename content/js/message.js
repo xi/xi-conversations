d19892fc36bfb12f989caf3c53f61898e9f2db55
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-09-06 05:35:00 +0200

    assign button events directly

--- content/js/message.js
+++ content/js/message.js
@@ -28,7 +28,7 @@ var autoMarkAsRead = function(e, msg) {
 	}, 100);
 
 	var onAction = function(event) {
-		if (event.target.closest('[data-action]')) {
+		if (event.target.closest('button')) {
 			actions.markAsRead(msg, true);
 			clear();
 		}
@@ -41,23 +41,31 @@ var autoMarkAsRead = function(e, msg) {
 	};
 };
 
+var toggleDropdown = function(msg, button) {
+	event.preventDefault();
+	var dropdown = button.parentElement.querySelector('.dropdown');
+	dropdown.classList.toggle('is-expanded');
+};
+
 export default function(msg, expanded) {
 	var h = util.h;
 	var _ = browser.i18n.getMessage;
 
 	var canReplyAll = msg.recipients.length + msg.ccList.length + msg.bccList.length > 1;
 
-	var createButton = function(attrs, icon, label, labelVisible) {
-		if (labelVisible) {
-			return h('button', Object.assign({'type': 'button'}, attrs), [util.createIcon(icon), ' ', label]);
-		} else {
-			return h('button', Object.assign({'type': 'button'}, attrs), [util.createIcon(icon, label)]);
-		}
+	var createButton = function(attrs, action, icon, label, labelVisible) {
+		var content = labelVisible ? [util.createIcon(icon), ' ', label] : [util.createIcon(icon, label)];
+		var button = h('button', Object.assign({'type': 'button'}, attrs), content);
+		button.addEventListener('click', event => {
+			event.preventDefault();
+			action(msg, button);
+		});
+		return button;
 	};
 
 	var e = h('article', {'class': expanded ? 'message is-expanded' : 'message', 'id': `msg-${msg.id}`, 'tabindex': -1}, [
 		h('header', {'class': 'message__header'}, [
-			createButton({'class': 'star', 'aria-pressed': msg.flagged, 'data-action': 'toggleFlagged'}, 'star', _('star')),
+			createButton({'class': 'star', 'aria-pressed': msg.flagged}, actions.toggleFlagged, 'star', _('star')),
 			...util.parseContacts([msg.author]).map(author => h('a', {
 				'class': 'message__author',
 				'href': `mailto:${author.email}`,
@@ -76,19 +84,19 @@ export default function(msg, expanded) {
 			util.createDate(msg.date),
 			h('span', {'class': 'message__actions'}, [
 				canReplyAll
-					? createButton({'class': 'button', 'data-action': 'replyAll'}, 'reply_all', _('replyAll'))
+					? createButton({'class': 'button'}, actions.replyAll, 'reply_all', _('replyAll'))
 					: msg.canReplyToList
-						? createButton({'class': 'button', 'data-action': 'replyToList'}, 'list', _('replyList'))
-						: createButton({'class': 'button', 'data-action': 'replyToSender'}, 'reply', _('reply')),
-				createButton({'class': 'button dropdownToggle'}, 'menu', _('more')),
+						? createButton({'class': 'button'}, actions.replyToList, 'list', _('replyList'))
+						: createButton({'class': 'button'}, actions.replyToSender, 'reply', _('reply')),
+				createButton({'class': 'button'}, toggleDropdown, 'menu', _('more')),
 				h('div', {'class': 'dropdown'}, [
-					createButton({'class': 'dropdown-item', 'data-action': 'replyToSender'}, 'reply', _('reply'), true),
-					canReplyAll ? createButton({'class': 'dropdown-item', 'data-action': 'replyAll'}, 'reply_all', _('replyAll'), true) : null,
-					msg.canReplyToList ? createButton({'class': 'dropdown-item', 'data-action': 'replyToList'}, 'list', _('replyList'), true) : null,
-					createButton({'class': 'dropdown-item', 'data-action': 'forward'}, 'forward', _('forward'), true),
-					createButton({'class': 'dropdown-item', 'data-action': 'editAsNew'}, 'create', _('edit'), true),
-					createButton({'class': 'dropdown-item', 'data-action': 'viewClassic'}, 'open_in_new', _('viewClassic'), true),
-					createButton({'class': 'dropdown-item', 'data-action': 'viewSource'}, 'code', _('viewSource'), true),
+					createButton({'class': 'dropdown-item'}, actions.replyToSender, 'reply', _('reply'), true),
+					canReplyAll ? createButton({'class': 'dropdown-item'}, actions.replyAll, 'reply_all', _('replyAll'), true) : null,
+					msg.canReplyToList ? createButton({'class': 'dropdown-item'}, actions.replyToList, 'list', _('replyList'), true) : null,
+					createButton({'class': 'dropdown-item'}, actions.forward, 'forward', _('forward'), true),
+					createButton({'class': 'dropdown-item'}, actions.editAsNew, 'create', _('edit'), true),
+					createButton({'class': 'dropdown-item'}, actions.viewClassic, 'open_in_new', _('viewClassic'), true),
+					createButton({'class': 'dropdown-item'}, actions.viewSource, 'code', _('viewSource'), true),
 				]),
 			]),
 		]),
@@ -115,28 +123,12 @@ export default function(msg, expanded) {
 	});
 
 	// dropdown events
-	var dropdownToggle = e.querySelector('.dropdownToggle');
 	var dropdown = e.querySelector('.dropdown');
-	dropdownToggle.addEventListener('click', function(event) {
-		event.preventDefault();
-		dropdown.classList.toggle('is-expanded');
-	});
 	document.addEventListener('focusout', function(event) {
 		if (!event.relatedTarget || !dropdown.contains(event.relatedTarget)) {
 			dropdown.classList.remove('is-expanded');
 		}
 	});
-
-	// action events
-	var buttons = e.querySelectorAll('[data-action]');
-	for (let i = 0; i < buttons.length; i++) {
-		const button = buttons[i];
-		const fn = actions[button.dataset.action];
-		button.addEventListener('click', function(event) {
-			event.preventDefault();
-			fn(msg, event.currentTarget);
-		});
-	}
 
 	// body
 	var footer = e.querySelector('.message__footer');
